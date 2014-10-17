088d3eec310e1321bf8411ee58608d7e743d77b1
Martin Bures <m2bures@gmail.com>
2014-10-17 18:34:54 +0200

    added Storage::exportFile();

--- app/lib/Storage.class.php
+++ app/lib/Storage.class.php
@@ -2,13 +2,32 @@
 /**
  * Storage class.
  *
- * @version 1.0
+ * @version 1.1
  * @author MPI
  *
  */
 class Storage{
     public static function exportFile($realName, $realPath, $exportName){
-        
+        $fullFilePath = $realPath . $realName;
+        $mimeType = self::getMimeType($fullFilePath);
+        header("Content-Type: " . $mimeType);
+        header("Content-Disposition: attachment; filename=\"" . $exportName . "\"");
+        header("Content-Transfer-Encoding: binary");
+        header("Accept-Ranges: bytes");
+        header("Cache-Control: private");
+        header("Pragma: private");
+        header("Expires: Sat, 01 Jan 2000 00:00:00 GMT");
+        readfile($fullFilePath);
     }  
+    
+    public static function getMimeType($file){
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $r = false;
+        if (is_resource($finfo) === true) {
+            $r = finfo_file($finfo, $file);
+        }
+        finfo_close($finfo);
+        return $r;
+    }
 }
 ?>
