79ddec98bbf20811d888676a595738b9f73cb3bf
Martin Bures <m2bures@gmail.com>
2014-10-17 17:45:06 +0200

    added Storage class;

---
+++ app/lib/Storage.class.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Storage class.
+ *
+ * @version 1.0
+ * @author MPI
+ *
+ */
+class Storage{
+    public static function exportFile($realName, $realPath, $exportName){
+        
+    }  
+}
+?>
