464056221d7ea979de0c85554d6863f0dbb66c72
Martin Bures <m2bures@gmail.com>
2014-10-27 20:09:57 +0100

    added Template class;

---
+++ app/Template.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Root template object.
+ *
+ * @version 1.0
+ * @author MPI
+ * */
+class Template {
+	private $data;
+
+	public function __construct($data) {
+		$this->data = $data;
+	}
+}
+?>
