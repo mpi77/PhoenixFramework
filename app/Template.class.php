976e0c7ad2fc77c22523eac7a3c24e5b5c9e7bfb
Martin Bures <m2bures@gmail.com>
2014-10-27 20:22:53 +0100

    added Template/get+set;

--- app/Template.class.php
+++ app/Template.class.php
@@ -3,14 +3,32 @@
 /**
  * Root template object.
  *
- * @version 1.0
+ * @version 1.1
  * @author MPI
  * */
 class Template {
-	private $data;
+    const NOT_FOUND = null;
+    
+    private $data;
 
-	public function __construct($data) {
-		$this->data = $data;
-	}
+    public function __construct($data = null) {
+        if (is_null($data)) {
+            $this->data = array ();
+        } else {
+            $this->data = $data;
+        }
+    }
+    
+    public function get($key){
+        if(array_key_exists($key, $this->data)){
+            return $this->data[$key];
+        } else{
+            return self::NOT_FOUND;
+        }
+    }
+    
+    public function set($key, $value){
+        $this->data[$key] = $value;
+    }
 }
 ?>
