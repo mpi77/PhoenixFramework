593074782eb851f5a33326c616fa85ac395fd702
Martin Bures <m2bures@gmail.com>
2014-10-30 21:50:57 +0100

    added breadcrumbs class feature;

---
+++ app/lib/Breadcrumbs.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Breadcrumbs class.
+ *
+ * @version 1.0
+ * @author MPI
+ *
+ */
+class Breadcrumbs {
+
+    public static function get($appendBefore = null, $appendAfter = null) {
+        return "not implemented yet";
+    }
+}
+?>
