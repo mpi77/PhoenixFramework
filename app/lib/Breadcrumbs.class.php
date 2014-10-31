81f6b6a2992db06865eb4f19b3752992386ce040
Martin Bures <m2bures@gmail.com>
2014-10-31 21:28:46 +0100

    updated default values in breadcrumbs;

--- app/lib/Breadcrumbs.class.php
+++ app/lib/Breadcrumbs.class.php
@@ -2,7 +2,7 @@
 /**
  * Breadcrumbs class.
  *
- * @version 1.3
+ * @version 1.4
  * @author MPI
  *
  */
@@ -12,14 +12,14 @@ class Breadcrumbs {
     }
 
     public static function get($routeName = null, $actionName = null, $appendBefore = null, $appendAfter = null) {
-        return self::makeBreadcrumbString($routeName, $actionName, $appendBefore, $appendAfter);
+        return self::makeBreadcrumbString(!is_null($routeName) ? $routeName : $_GET["route"], !is_null($actionName) ? $actionName : $_GET["action"], $appendBefore, $appendAfter);
     }
 
     private static function makeBreadcrumbString($routeName = null, $actionName = null, $appendBefore = null, $appendAfter = null, $styleBoxId = "page-breadcrumb", $styleOlClass = "breadcrumb", $styleActiveClass = "active") {
         $r = sprintf("<div id=\"%s\"><ol class=\"%s\">", $styleBoxId, $styleOlClass);
         $r .= !is_null($appendBefore) ? $appendBefore : "";
         $r .= sprintf("<li><a href=\"%s\">%s</a></li>", Config::SITE_PATH, "Home");
-        if (!empty($routeName) && !empty($actionName) && Router::isRoute($routeName)) {
+        if (!empty($routeName) && !empty($actionName) && Router::isRoute($routeName) && $routeName != Router::DEFAULT_EMPTY_ROUTE) {
             $route = Router::getRoute($routeName);
             $r .= sprintf("<li><a href=\"%s\">%s</a></li>", $route->getLinkUrl(), $route->getLinkBody());
             if ($route->isAction($actionName)) {
