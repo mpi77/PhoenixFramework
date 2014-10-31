ad15357d43da1def5270517da25a30f3b6ebda92
Martin Bures <m2bures@gmail.com>
2014-10-31 19:39:34 +0100

    added breadcrumbs generation;

--- app/lib/Breadcrumbs.class.php
+++ app/lib/Breadcrumbs.class.php
@@ -2,7 +2,7 @@
 /**
  * Breadcrumbs class.
  *
- * @version 1.1
+ * @version 1.2
  * @author MPI
  *
  */
@@ -10,9 +10,21 @@ class Breadcrumbs {
 
     private function __construct() {
     }
-    
-    public static function get($appendBefore = null, $appendAfter = null) {
-        return "not implemented yet";
+
+    public static function get($routeName, $actionName, $appendBefore = null, $appendAfter = null) {
+        return self::makeBreadcrumbString($routeName, $actionName, $appendBefore, $appendAfter);
+    }
+
+    private static function makeBreadcrumbString($routeName, $actionName, $appendBefore = null, $appendAfter = null, $styleBoxId = "page-breadcrumb", $styleOlClass = "breadcrumb") {
+        $route = Router::getRoute($routeName);
+        $r = sprintf("<div id=\"%s\"><ol class=\"%s\">", $styleBoxId, $styleOlClass);
+        $r .= !is_null($appendBefore) ? $appendBefore : "";
+        $r .= sprintf("<li><a href=\"%s\">%s</a></li>", Config::SITE_PATH, "Home");
+        $r .= sprintf("<li><a href=\"%s\">%s</a></li>", $route->getLinkUrl(), $route->getLinkBody());
+        $r .= sprintf("<li><a href=\"%s\">%s</a></li>", $route->getAction($actionName)->getLinkUrl(), $route->getAction($actionName)->getLinkBody());
+        $r .= !is_null($appendAfter) ? $appendAfter : "";
+        $r .= "</ol></div>";
+        return $r;
     }
 }
 ?>
