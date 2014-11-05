2d8ddf63faffbd45eae6e210515aa08f71f55205
Martin Bures <m2bures@gmail.com>
2014-11-05 23:37:41 +0100

    added validationTable into UserController;

--- app/controller/UserController.class.php
+++ app/controller/UserController.class.php
@@ -4,11 +4,12 @@ Router::register("user", new Route("UserModel", "UserView", "UserController", ar
 /**
  * User controller.
  *
- * @version 1.1
+ * @version 1.2
  * @author MPI
  *        
  */
 class UserController extends Controller {
+    private static $validationTable = array ();
 
     public function __construct(Model $model, $args) {
         parent::__construct($model, $args);
@@ -16,6 +17,10 @@ class UserController extends Controller {
 
     public function getName() {
         return get_class($this);
+    }
+
+    public static function getRegexp($key) {
+        return self::$validationTable[$key];
     }
 }
 ?>
