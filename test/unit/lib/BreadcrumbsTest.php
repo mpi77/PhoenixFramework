7a0069e6249949437721d4545310a44003fed4eb
Martin Bures <m2bures@gmail.com>
2014-11-13 00:12:48 +0100

    added breadcrumb item tests;

--- test/unit/lib/BreadcrumbsTest.php
+++ test/unit/lib/BreadcrumbsTest.php
@@ -2,7 +2,7 @@
 /**
  * Breadcrumbs unit test.
  *
- * @version 1.0
+ * @version 1.1
  * @author MPI
  * */
 include '../../app/lib/Breadcrumbs.class.php';
@@ -15,8 +15,50 @@ class BreadcrumbsTest extends PHPUnit_Framework_TestCase {
 
     public function testEmptyConstructor() {
         $this->breadcrumbItem = new Breadcrumbs(null, null);
-        $this->assertNull($this->breadcrumbItem->getBody());
         $this->assertNull($this->breadcrumbItem->getUrl());
+        $this->assertNull($this->breadcrumbItem->getBody());
+        $this->assertNull($this->breadcrumbItem->getTitle());
+    }
+    
+    public function testValidEmptyUrlBody() {
+        $this->breadcrumbItem = new Breadcrumbs("", 15);
+        $this->assertEquals("", $this->breadcrumbItem->getUrl());
+        $this->assertEquals(15, $this->breadcrumbItem->getBody());
+        $this->assertNull($this->breadcrumbItem->getTitle());
+    }
+    
+    public function testValidUrlBody() {
+        $this->breadcrumbItem = new Breadcrumbs("user/example/", 15);
+        $this->assertEquals("user/example/", $this->breadcrumbItem->getUrl());
+        $this->assertEquals(15, $this->breadcrumbItem->getBody());
+        $this->assertNull($this->breadcrumbItem->getTitle());
+    }
+    
+    public function testValidUrlBodyTitle() {
+        $this->breadcrumbItem = new Breadcrumbs("user/example/", 15, 55);
+        $this->assertEquals("user/example/", $this->breadcrumbItem->getUrl());
+        $this->assertEquals(15, $this->breadcrumbItem->getBody());
+        $this->assertEquals(55, $this->breadcrumbItem->getTitle());
+    }
+    
+    public function testValidUrlInvalidBodyTitle() {
+        $this->breadcrumbItem = new Breadcrumbs("user/example/", null, 0.0);
+        $this->assertEquals("user/example/", $this->breadcrumbItem->getUrl());
+        $this->assertNull($this->breadcrumbItem->getBody());
+        $this->assertNull($this->breadcrumbItem->getTitle());
+    }
+    
+    public function testValidUrlBodyInvalidTitle() {
+        $this->breadcrumbItem = new Breadcrumbs("user/example/", 15, 0.0);
+        $this->assertEquals("user/example/", $this->breadcrumbItem->getUrl());
+        $this->assertEquals(15, $this->breadcrumbItem->getBody());
+        $this->assertNull($this->breadcrumbItem->getTitle());
+    }
+    
+    public function testInvalidUrlBodyTitle() {
+        $this->breadcrumbItem = new Breadcrumbs(null, true, 0.0);
+        $this->assertNull($this->breadcrumbItem->getUrl());
+        $this->assertNull($this->breadcrumbItem->getBody());
         $this->assertNull($this->breadcrumbItem->getTitle());
     }
 }
