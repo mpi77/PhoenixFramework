60f98dd582d907b815287fa1b4d3641911a9bf20
Martin Bures <m2bures@gmail.com>
2014-11-17 22:08:53 +0100

    added testSimpleDataStore();

--- test/unit/TemplateDataTest.php
+++ test/unit/TemplateDataTest.php
@@ -2,7 +2,7 @@
 /**
  * Template data unit test.
  *
- * @version 1.0
+ * @version 1.1
  * @author MPI
  * */
 include '../../app/TemplateData.class.php';
@@ -12,10 +12,25 @@ class TemplateDataTest extends PHPUnit_Framework_TestCase {
     protected function setUp() {
         $this->data = null;
     }
-    
+
     public function testEmptyConstructor() {
         $this->data = new TemplateData();
         $this->assertEmpty($this->data->get());
+        $this->assertEmpty($this->data->getAll());
+    }
+
+    public function testSimpleDataStore() {
+        $a = array (
+                        "key" => "value",
+                        "key2" => "value2",
+                        "key3" => "value3" 
+        );
+        $this->data = new TemplateData($a);
+        $this->assertEquals("value", $this->data->get("key"));
+        $this->assertEquals("value2", $this->data->get("key2"));
+        $this->assertEquals("value3", $this->data->get("key3"));
+        $this->assertSame($a, $this->data->getAll());
+        
     }
 }
 ?>
