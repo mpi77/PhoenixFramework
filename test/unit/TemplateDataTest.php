84a004c36fa0269d640a2b9daf03b3f59f4a8ece
Martin Bures <m2bures@gmail.com>
2014-11-17 22:38:12 +0100

    updated TemplateDataTest/testSimpleDataStore();

--- test/unit/TemplateDataTest.php
+++ test/unit/TemplateDataTest.php
@@ -2,7 +2,7 @@
 /**
  * Template data unit test.
  *
- * @version 1.2
+ * @version 1.3
  * @author MPI
  * */
 include '../../app/TemplateData.class.php';
@@ -34,22 +34,31 @@ class TemplateDataTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals("value3", $this->data->get("key3"));
         $this->assertSame($a, $this->data->getAll());
         
+        /* merge array a + b */
         $b = array (
-                        "key4" => "value4" 
+                        "key4" => "value4",
+                        "key6" => "val<u>e6"
         );
         $a = array_merge($a, $b);
         $this->data->set("key4", "value4");
-        
+        $this->data->set("key6", "val<u>e6");
         $this->assertTrue($this->data->has("key4"));
+        $this->assertTrue($this->data->has("key6"));
         $this->assertEquals("value4", $this->data->get("key4"));
+        $this->assertEquals("val<u>e6", $this->data->get("key6"));
         $this->assertSame($a, $this->data->getAll());
         
+        /* change value on key4 */
         $this->data->set("key4", "value5");
         $a["key4"] = "value5";
-        
         $this->assertEquals("value5", $this->data->get("key4"));
         $this->assertSame($a, $this->data->getAll());
         
+        /* print values */
+        $this->expectOutputString("val<u>e6");
+        $this->data->e("key6");
+        
+        /* get values from non-string keys */
         $this->assertNull($this->data->get(true));
         $this->assertNull($this->data->get(2.0));
         $this->assertNull($this->data->get($this->getMock("ExampleObj")));
