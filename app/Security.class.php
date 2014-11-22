4534be006b08bc39d434617fe79b87bb65939e2e
Martin Bures <m2bures@gmail.com>
2014-11-22 22:14:25 +0100

    added Security class;

---
+++ app/Security.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Security
+ * 
+ * @version 1.0
+ * @author MPI
+ * */
+class Security {
+
+    public function __construct() {
+    }
+
+    public function __destruct() {
+    }
+}
+?>
