c4d701964f124dd7ac8f3560f3e3c516ee713979
Martin Bures <m2bures@gmail.com>
2015-02-11 22:15:50 +0100

    implemented SimpleLoader;

--- Phoenix/Loaders/SimpleLoader.php
+++ Phoenix/Loaders/SimpleLoader.php
@@ -1,19 +1,42 @@
 <?php
+
 namespace Phoenix\Loaders;
 
 /**
  * SimpleLoader
  *
- * @version 1.0
+ * @version 1.1
  * @author MPI
- * */
-class SimpleLoader{
-    
+ *        
+ */
+class SimpleLoader {
+    const PHP_SUFFIX = ".php";
+    private static $filesOk = 0;
+    private static $filesFail = 0;
+
     /**
-     * @todo implementation
-     * 
-     * @param string $className
+     * Load given class name.
+     *
+     * @param string $className            
      */
-    public static function load($className){}
+    public static function load($className) {
+        $file = str_replace("\\", "/", $className) . self::PHP_SUFFIX;
+        if (file_exists($file)) {
+            include ($file);
+            if (class_exists($className)) {
+                self::$filesOk++;
+                return true;
+            }
+        }
+        self::$filesFail++;
+        return false;
+    }
+
+    public static function getStats() {
+        return array (
+                        "filesOk" => self::$filesOk,
+                        "filesFail" => self::$filesFail 
+        );
+    }
 }
 ?>
