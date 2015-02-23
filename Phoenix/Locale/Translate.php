5d6f658ab980d32153ea020ed89ba8f2bfc937a7
Martin Bures <m2bures@gmail.com>
2015-02-23 16:45:20 +0100

    added $translator_module validation in Translate::get(..); commented Translate;

--- Phoenix/Locale/Translate.php
+++ Phoenix/Locale/Translate.php
@@ -9,7 +9,7 @@ use \App\AppTranslator;
 /**
  * Translate is wrapper for ModuleTranslators.
  *
- * @version 1.8
+ * @version 1.9
  * @author MPI
  *        
  */
@@ -21,26 +21,35 @@ class Translate {
     /**
      * Get string or string pattern from ModuleTranslator.
      *
-     * @param string $translator_module            
+     * @param string $translator_module
+     *            call \App\Locale\Def\ModuleDefinition::getModuleName()
      * @param integer $key
      *            constant defined in \App\Locale\Def\ModuleDefinition
-     * @param string $language        
-     *     
+     * @param string $language
+     *            language prefix defined as IApplicationTranslator::LANG_PREFIX index in AppTranslator::$languages[i]; if is null then default language is selected
+     *            
      * @return string
      */
     public static function get($translator_module, $key, $language = null) {
+        if (empty($translator_module)) {
+            return IModuleTranslator::DEFAULT_VALUE;
+        }
         if (empty($language)) {
             $language = AppTranslator::getDefaultLanguage()[IApplicationTranslator::LANG_PREFIX];
         }
         $t = "\App\Locale\Lang\\" . $language . "\\" . $translator_module . "Translator";
-        return $t::get($key);
+        return (class_exists($t)) ? $t::get($key) : IModuleTranslator::DEFAULT_VALUE;
     }
 
     /**
      * Print htmlspecialchars(string) from ModuleTranslator.
      *
+     * @param string $translator_module
+     *            call \App\Locale\Def\ModuleDefinition::getModuleName()
      * @param integer $key
-     *            constant defined in AppTranslator
+     *            constant defined in \App\Locale\Def\ModuleDefinition
+     * @param string $language
+     *            language prefix defined as IApplicationTranslator::LANG_PREFIX index in AppTranslator::$languages[i]; if is null then default language is selected
      */
     public static function es($translator_module, $key, $language = null) {
         echo htmlspecialchars(self::get($translator_module, $key, $language));
@@ -49,8 +58,12 @@ class Translate {
     /**
      * Print string from actual Translator.
      *
+     * @param string $translator_module
+     *            call \App\Locale\Def\ModuleDefinition::getModuleName()
      * @param integer $key
-     *            constant defined in AppTranslator
+     *            constant defined in \App\Locale\Def\ModuleDefinition
+     * @param string $language
+     *            language prefix defined as IApplicationTranslator::LANG_PREFIX index in AppTranslator::$languages[i]; if is null then default language is selected
      */
     public static function e($translator_module, $key, $language = null) {
         echo self::get($translator_module, $key, $language);
